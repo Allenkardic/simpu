fa542932ee880ff2621cbaa17e75c79faaa7fbeb
Aliakwe <aliakwe@yahoo.com>
2020-02-17 11:14:18 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Rectangle">hello</div>
+    </div>
+  );
+}
+
+export default App;
